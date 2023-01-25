946d199fd5f0a8fc877ca336de7ff733cf58aa1f
Matteo Greco <matteogreco9@gmail.com>
2023-01-25 16:55:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='jdtree',
     packages=['jdtree'],
-    version='0.0.2',
+    version='0.0.3',
     description='Json Decision Table/Tree',
     author='Matteo Greco',
     license='MIT',
